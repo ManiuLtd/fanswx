7bc5a73cdf90c35322c5f145392eb709bcc51a52
JK <291266278@qq.com>
2018-11-22 13:08:11 +0800

    update api route

--- routes/api.php
+++ routes/api.php
@@ -18,16 +18,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::namespace('Api')->group(function () {
-    Route::any('/serve', 'WeChatController@serve');
+    Route::any('/serve', 'WechatController@serve');
     //更新菜单
-    Route::get('/menu/update', 'WeChatController@menu_update');
+    Route::get('/menu/update', 'WechatController@menu_update');
     //生成带参数临时二维码    参数格式id/有效天数 T_xxx/1-30
-    Route::get('/qrcode/temporary/{scene_id}/{day}', 'WeChatController@qrcode_temporary')
+    Route::get('/qrcode/temporary/{scene_id}/{day}', 'WechatController@qrcode_temporary')
         ->where([
             'scene_id' => '^(T_)[a-z0-9]+',
             'day' => '^([12][0-9]|30|[1-9])$',
         ]);
     //生成带参数永久二维码 参数格式id /F_xxx
-    Route::get('/qrcode/forever/{scene_id}', 'WeChatController@qrcode_forever')
+    Route::get('/qrcode/forever/{scene_id}', 'WechatController@qrcode_forever')
         ->where('scene_id', '^(F_)[a-z0-9]+');
 });
